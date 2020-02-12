f649c4987995a25bb346e540413068a1aa8dff35
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-12 18:39:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -22,7 +22,7 @@ end
 
 def speak_to_grandma(phrase)
   if (phrase=="#{phrase}".upcase)
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
